ff398a2cd264661cccb5d00478211336521c9586
Saito Shunsuke <50902764+syun100000@users.noreply.github.com>
2024-12-04 09:22:03 +0900

    ビデオモデルにタグと出演者の多対多関係を追加し、出演者モデルの名前保存処理を改善

--- player/models.py
+++ player/models.py
@@ -4,15 +4,17 @@ from django.db import models
 
 # ビデオ
 class Video(models.Model):
-    id = models.AutoField(primary_key=True, unique=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=1000)
     description = models.TextField()
     number = models.IntegerField(null=True)
     path = models.CharField(max_length=1000)
+    tags = models.ManyToManyField('Tag', related_name='videos')  # タグとの多対多関係
+    actors = models.ManyToManyField('Actor', related_name='videos')  # 出演者との多対多関係
 
 # タグ
 class Tag(models.Model):
-    name = models.CharField(max_length=1000, primary_key=True, unique=True)
+    name = models.CharField(max_length=1000, primary_key=True)
     description = models.TextField(null=True)
 
 # 出演者
@@ -22,7 +24,7 @@ class Actor(models.Model):
     birth = models.DateField(null=True)
     
     def save(self, *args, **kwargs):
-        self.name = self.name.lower()   # 名前を小文字に変換
+        self.name = self.name.lower()   # 名前を小文字に変換する
         # 空白を削除
         self.name = self.name.replace(" ", "")
         # 全角スペースを削除
